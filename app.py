b276a3476a4f7650d04415b21560027095fbe31a
LJiaqingNick <gmziue@gmail.com>
2023-10-19 10:54:09 -0700

    implementing the scoring system

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def load_landmarks():
 
 def main():
     st.title("Guess the Landmark")
+    score_placeholder = st.empty()
 
     if 'current_landmark_index' not in st.session_state:
         st.session_state.current_landmark_index = 0
@@ -16,15 +17,25 @@ def main():
         st.session_state.hint_index = 0
     if "landmarks" not in st.session_state:
         st.session_state.landmarks = load_landmarks()
+    if "score" not in st.session_state:
+        st.session_state.score = 0
+    if "current_landmark_success" not in st.session_state:
+        st.session_state.current_landmark_success = False
 
 
+    score_placeholder.write(f"Score: {st.session_state.score}")
     guess = st.text_input("Your Guess:")
 
     if st.button("Submit"):
-        if guess.strip().lower() == st.session_state.landmarks[st.session_state.current_landmark_index]["name"].lower():
-            st.success("That's the right answer!")
-        else:
-            st.error("Try again or ask for another hint!")
+        if not st.session_state.current_landmark_success:
+            if guess.strip().lower() == st.session_state.landmarks[st.session_state.current_landmark_index]["name"].lower():
+                st.success("That's the right answer!")
+                st.session_state.score += (3 - st.session_state.hint_index)
+                st.session_state.current_landmark_success = True
+                # update the score
+                score_placeholder.write(f"Score: {st.session_state.score}")
+            else:
+                st.error("Try again or ask for another hint!")
 
     if st.button("Next Hint"):
         if 'hint_index' in st.session_state:
@@ -51,6 +62,7 @@ def move_to_next_landmark():
             st.session_state.current_landmark_index = len(st.session_state.landmarks) - 1  # Stay on the last landmark
             st.balloons()
             st.write("Congratulations! You've completed the game!")
+        st.session_state.current_landmark_success = False
 
 if __name__ == "__main__":
     main()
